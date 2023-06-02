202c70a7389b2c56732187ba937fdf56995324c0
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-06-03 02:09:11 +0300

    improve style

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function addTask(){
 
     if(name!=""){
 
-        tasks.push({"name":name , "priority":priority , "inEdit":0});
+        tasks.push({name :name , priority :priority, eName :name , ePriority :priority , inEdit :0});
 
         filter();
     }
@@ -35,27 +35,42 @@ function tableRender(){
 
     for (let i=0;i<tasks.length;i++){
 
+        let tsk = tasks[i];
+
         taskHtm+= `<tr>
         <td>${i+1}</td>
-        <td class="${tasks[i].inEdit? "d-none":""}">${tasks[i].name}</td>
-        <td class="${tasks[i].inEdit? "d-none":""}">${tasks[i].priority}</td>
-        <td class="${tasks[i].inEdit? "":"d-none"}" "><input id="task-name-i-${i}" type="text" value="${tasks[i].name}"></td>
-        <td class="${tasks[i].inEdit? "":"d-none"}" >
-        <select name="priority" id="priority-i-${i}">
-        <option value="Low" ${tasks[i].priority=='Low'? 'selected':''}>Low</option>
-        <option value="Medium" ${tasks[i].priority=='Medium'? 'selected':''}>Medium</option>
-        <option value="High" ${tasks[i].priority=='High'? 'selected':''}>High</option>
+        <td class="${tsk.inEdit? "d-none":""}">${tsk.name}</td>
+        <td class="${tsk.inEdit? "d-none":""}">${tsk.priority}</td>
+        <td class="${tsk.inEdit? "":"d-none"}" >
+        <input id="task-name-i-${i}" type="text" value="${tsk.eName}" onchange="eName(${i},this)">
+        </td>
+        <td class="${tsk.inEdit? "":"d-none"}" >
+        <select name="priority" id="priority-i-${i}" onchange="ePriority(${i},this)">
+        <option value="Low" ${tsk.ePriority=='Low'? 'selected':''}>Low</option>
+        <option value="Medium" ${tsk.ePriority=='Medium'? 'selected':''}>Medium</option>
+        <option value="High" ${tsk.ePriority=='High'? 'selected':''}>High</option>
         </select>
         </td>
         <td>
-            <button class="edit ${tasks[i].inEdit? "d-none":""}" onclick="edit(${i})">Edit</button>
-            <button class="edit ${tasks[i].inEdit? "":"d-none"}" onclick="saveEdits(${i})">Save Edits</button>
-            <button class="edit ${tasks[i].inEdit? "":"d-none"}" onclick="cancelEdits(${i})">Cancel Edits</button>
-            <button class="delete ${tasks[i].inEdit? "d-none":""}" onclick="deleteTask(${i})">Delete</button>
+            <button class="edit ${tsk.inEdit? "d-none":""}" onclick="edit(${i})">Edit</button>
+            <button class="edit ${tsk.inEdit? "":"d-none"}" onclick="saveEdits(${i})">Save Edits</button>
+            <button class="edit ${tsk.inEdit? "":"d-none"}" onclick="cancelEdits(${i})">Cancel Edits</button>
+            <button class="delete ${tsk.inEdit? "d-none":""}" onclick="deleteTask(${i})">Delete</button>
         </td> 
         </tr>`;
 
     }
+    
+
+    taskHtm+= ( tasks.reduce((t,n)=>t+n.inEdit,0) >1 )? 
+        `<tr>
+            <td colspan="3"></td>
+            <td>
+                <button class="edit" onclick="saveAll()">Save All</button>
+            </td>
+        </tr>`
+        :``;
+
 
     tBody.innerHTML=taskHtm;
 }
@@ -84,15 +99,30 @@ function edit(i){
     tableRender();
 }
 
+function eName(i,e){
+    tasks[i].eName = e.value;
+}
+function ePriority(i,e){
+    tasks[i].ePriority = e.value;
+}
+
 function saveEdits(i){
 
     tasks[i].inEdit=0;
 
-    let name=document.getElementById("task-name-i-"+i).value;
-    let priority=document.getElementById("priority-i-"+i).value;
+    tasks[i].name=tasks[i].eName;
+    tasks[i].priority=tasks[i].ePriority;
 
-    tasks[i].name=name;
-    tasks[i].priority=priority;
+    tableRender();
+}
+
+function saveAll(){
+
+    for (let i=0;i<tasks.length;i++){
+        if(tasks[i].inEdit){
+            saveEdits(i);
+        }
+    }
 
     tableRender();
 }
@@ -101,5 +131,9 @@ function cancelEdits(i){
 
     tasks[i].inEdit=0;
 
+    tasks[i].eName=tasks[i].name;
+    tasks[i].ePriority=tasks[i].priority;
+
     tableRender();
 }
+
